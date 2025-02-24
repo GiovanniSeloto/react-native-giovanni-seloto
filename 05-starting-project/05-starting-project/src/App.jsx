dd0c7b814c9522f8f718e0f7ad81a3453b09d8de
Giovanni Seloto <giovanni.seloto.silva@gmail.com>
2025-02-24 13:54:50 -0300

    Novas atualizações

--- 05-starting-project/05-starting-project/src/App.jsx
+++ 05-starting-project/05-starting-project/src/App.jsx
@@ -1,10 +1,21 @@
 import Home from "./Home";
-import Sidebar from "./Components/Sidebar";
+import Form from "./Components/Forms";
+import { useState } from "react";
+
 function App() {
+
+  const [isSelected, setIsSelected] = useState("Home")
+
   return (
     <main  className="h-screen my-8 flex gap-8">
-      <Sidebar/>
-      <Home/>
+        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
+            <ul className="mt-8">...</ul>
+            <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                onClick={()=> setIsSelected("Form")}
+            >+ Add Project</button>
+        </aside>
+      {isSelected === "Home" ? <Home/> : <Form/>}
     </main>
   );
 }
--- 05-starting-project/05-starting-project/src/Components/Forms.jsx
+++ 05-starting-project/05-starting-project/src/Components/Forms.jsx
@@ -1,16 +1,68 @@
-import createPortal from 'react-dom'
+import { useState } from "react"
 
-export default function Forms(){
-    return createPortal(
-        <form className="mt-4 text-right">
-            <button>Cancel</button>
-            <button>Save</button>
-            <label className="text-sm font-bold uppercase text-stone-500">title</label>
-            <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" type="text"/>
-            <label className="text-sm font-bold uppercase text-stone-500">Description</label>
-            <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" type="" />
-            <label className="text-sm font-bold uppercase text-stone-500">Due Date</label>
-            <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" type="date" />
-        </form>
-    ), document.querySelector("Home")
-}
+export default function Form() {
+
+    const [newButton, setNewButton] = useState(createNewButton());
+    const [forms, setForms] = useState({
+        title: 'Name project',
+        description: 'Description project',
+        date: ''
+    })
+
+    function createNewButton() {
+        const initialbutton = [];
+        for (let i = 0; i <= 1; i++) {
+            initialbutton.push({
+                id: i,
+                title: 'Item' + (i + 1)
+            });
+        }
+        return initialbutton;
+    }
+
+    function handleNewButton() {
+        setNewButton([
+            ...newButton,
+            {
+                id: newButton, 
+                title: forms.title
+            }
+        ])
+    }
+
+        return (
+            <form className="mt-4 text-right ">
+                <button button="true" className="text-stone-800 hover:text-stone-950">
+                    Cancel
+                </button>
+                <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleNewButton}>Save</button>
+                <div className="w-[35rem] mt-16 text-left">
+                    <label className="text-sm font-bold uppercase text-stone-500">title</label>
+                    <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+                        type="text"
+                        value={forms.title}
+                        onChange={e => {
+                            setForms({
+                                ...forms,
+                                title: e.target.value
+                            })
+                        }}
+                    />
+                    <label className="text-sm font-bold uppercase text-stone-500">Description</label>
+                    <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+                        type="text"
+                        value={forms.description}
+                        onChange={e => {
+                            setForms({
+                                ...forms,
+                                description: e.target.value
+                            })
+                        }}
+                    />
+
+                    <label className="text-sm font-bold uppercase text-stone-500">Due Date</label>
+                    <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" type="date" />
+                </div>
+            </form>
+        )
+    }
